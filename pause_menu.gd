034b4ff43883fe8459c8f5f73cd9299eaf7e3a3f
Ap0loVictor <apolovictorj.c@gmail.com>
2025-11-17 14:40:03 -0300

    Botão de pause funcionando

--- pause_menu.gd
+++ pause_menu.gd
@@ -1,14 +1,33 @@
 extends Control
 
+func _ready():
+	$AnimationPlayer.play("RESET")
+
 func resume():
 	get_tree().paused = false
-	
+	$AnimationPlayer.play_backwards("blur")
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func pause():
+	get_tree().paused = true
+	$AnimationPlayer.play("blur")
+
+func testEsc():
+	if (Input.is_action_just_pressed("pause") and !get_tree().paused ) :
+		pause()
+	elif (Input.is_action_just_pressed("pause") and get_tree().paused):
+		resume()
+
+func _on_resume_pressed() -> void:
+	resume()
+
+func _on_options_pressed() -> void:
 	pass # Replace with function body.
 
+func _on_main_menu_pressed() -> void:
+	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_exit_game_pressed() -> void:
+	get_tree().quit()
+
+func _process(delta):
+	testEsc()
