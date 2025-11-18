088a8b627763a9125856a6a3fce8ed856ff1d1f7
Ap0loVictor <apolovictorj.c@gmail.com>
2025-11-18 15:11:53 -0300

    Removido resolução do zoom

--- scripts/menus/main_menu.gd
+++ scripts/menus/main_menu.gd
@@ -12,8 +12,8 @@ func _process(delta: float) -> void:
 
 
 func _on_play_pressed() -> void:
-	#get_tree().change_scene_to_file("res://scenes/main.tscn")
-	SceneManager.go_to_game()
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
 
 
 func _on_achievements_pressed() -> void:
