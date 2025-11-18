cd29ad03b3fb97446100a27ee6b27d6b56f005be
Vinícius Campelo <antonio123campelo@gmail.com>
2025-11-18 10:57:52 -0300

    Add BarHP
    adicionado mas não muda as cores dinamicamente ainda

--- scripts/BattleUI.gd
+++ scripts/BattleUI.gd
@@ -20,7 +20,7 @@ extends Control
 var state: String = "message"
 
 func _ready():
-
+	update_status_boxes()
 	# Conectar botões do menu principal
 	btn_fight.pressed.connect(_on_fight)
 	btn_bag.pressed.connect(_on_bag)
@@ -85,8 +85,66 @@ func _on_party():
 
 #  AÇÕES DOS BOTÕES DE ATAQUE
 func _on_attack_pressed(attack_name: String):
-	show_message("Você usou " + attack_name + "!")
+	damage_enemy(10)
+	show_message("Você usou %s!" % attack_name)
 
 
 func _on_back():
 	show_actions()
+
+@onready var player_name = $PlayerStatusBox/LabelName
+@onready var player_hp_label = $PlayerStatusBox/LabelHP
+@onready var player_hp_bar = $PlayerStatusBox/ProgressBarHP
+
+@onready var enemy_name = $EnemyStatusBox/LabelName
+@onready var enemy_hp_label = $EnemyStatusBox/LabelHP
+@onready var enemy_hp_bar = $EnemyStatusBox/ProgressBarHP
+
+var player_max_hp = 100
+var player_hp = 100
+
+var enemy_max_hp = 40
+var enemy_hp = 40
+
+func update_status_boxes():
+	
+	update_hp_color(player_hp_bar, player_hp, player_max_hp)
+	update_hp_color(enemy_hp_bar, enemy_hp, enemy_max_hp)
+
+	# Player
+	player_name.text = "Marquinhos"
+	player_hp_label.text = str(player_hp, " / ", player_max_hp)
+	player_hp_bar.max_value = player_max_hp
+	player_hp_bar.value = player_hp
+
+	# Enemy
+	enemy_name.text = "Amogus"
+	enemy_hp_label.text = str(enemy_hp, " / ", enemy_max_hp)
+	enemy_hp_bar.max_value = enemy_max_hp
+	enemy_hp_bar.value = enemy_hp
+
+func damage_enemy(amount: int):
+	enemy_hp = max(0, enemy_hp - amount)
+	update_status_boxes()
+
+func damage_player(amount: int):
+	player_hp = max(0, player_hp - amount)
+	update_status_boxes()
+
+func update_hp_color(pb: ProgressBar, hp: int, maxhp: int):
+	var percent := float(hp) / float(maxhp)
+
+	var color: Color
+
+	if percent > 0.5:
+		color = Color(0.0, 0.8, 0.0) # verde
+	elif percent > 0.2:
+		color = Color(1.0, 0.7, 0.0) # amarelo
+	else:
+		color = Color(1.0, 0.2, 0.2) # vermelho
+
+	var fill_style := StyleBoxFlat.new()
+	fill_style.bg_color = color
+
+	# ESTILO CORRETO DO FILL → "fg"
+	pb.add_theme_stylebox_override("fg", fill_style)
