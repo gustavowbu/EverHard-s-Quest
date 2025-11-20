6fe8626f3d5bbf064a96ecf24b2131c0f9630087
PudimDeOvo <183882067+PudimDeOvo@users.noreply.github.com>
2025-11-20 14:38:04 -0300

    fix: change from tileMap to tileMapLayer

---
+++ scenes/texture_rect.gd
@@ -0,0 +1,9 @@
+extends CanvasLayer
+
+var next_scene_path: String
+
+func start_transition(next_scene: String):
+	next_scene_path = next_scene
+	self.visible = true
+	await get_tree().create_timer(0.6).timeout
+	get_tree().change_scene_to_file(next_scene_path)
