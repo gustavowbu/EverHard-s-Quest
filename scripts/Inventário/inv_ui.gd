793056cb0ac4a091e8e630469346a5f3a6f4713e
Apolo Victor <apolovictorj.c@gmail.com>
2025-12-18 17:58:07 -0300

    Putting objecs in red inventory blocks and normal inventory blocs

--- scripts/Inventário/inv_ui.gd
+++ scripts/Inventário/inv_ui.gd
@@ -2,20 +2,22 @@ extends Control
 
 @onready var inv: Inv = preload("res://scenes/Inventário/inventory/Inventario.tres")
 @onready var slots: Array = $NinePatchRect/GridContainer.get_children()
-@onready var selectedObjecs: Inv = preload("res://scenes/Inventário/inventory/selectedObjects.tres")
+@onready var selectedObjects: Inv = preload("res://scenes/Inventário/inventory/selectedObjects.tres")
 @onready var red_slots: Array = $NinePatchRect/GridContainer2.get_children()
 
 var is_open = false
 
 func _ready():
+	inv.update.connect(update_slots)
+	selectedObjects.update.connect(update_slots)
 	update_slots()
 	close()
 
 func update_slots():
-	for i in range(min(selectedObjecs.items.size(),red_slots.size())):
-		red_slots[i].update(selectedObjecs.items[i])
-	for i in range(min(inv.items.size(),slots.size())):
-		slots[i].update(inv.items[i])
+	for i in range(min(inv.slots.size(),slots.size())):
+		slots[i].update(inv.slots[i])
+	for i in range(min(selectedObjects.slots.size(),red_slots.size())):
+		red_slots[i].update(selectedObjects.slots[i])
 
 func _process(delta):
 	if Input.is_action_just_pressed("inventory"):
